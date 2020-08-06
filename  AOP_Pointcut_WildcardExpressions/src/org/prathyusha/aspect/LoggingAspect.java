09c6ce6d770cceb892127aa0c26dd771996750cc
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-06 20:01:29 +0530

    Add files via upload

---
+++  AOP_Pointcut_WildcardExpressions/src/org/prathyusha/aspect/LoggingAspect.java
@@ -0,0 +1,37 @@
+package org.prathyusha.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class LoggingAspect {
+
+	//@Before("execution(public String org.prathyusha.model.Circle.getName())")
+	//@Before("execution(public * get*())")
+	//@Before("execution(* get*(..))")
+	//@Before("execution(* org.prathyusha.model.*.get*())")
+	
+	//@Before("execution(* get*())")
+	@Before("allGetters() && allCircleMethods()")
+	public void LoggingAdvice(){
+		System.out.println("Advice run, Get Method called");
+	}
+	
+	//@Before("execution(* get*())")
+	@Before("allGetters()")
+	public void secondAdvice(){
+		System.out.println("Second Advice executed");
+	}
+	
+	//Dummy Method
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}	
+	
+	@Pointcut("within(org.prathyusha.model.Circle)")
+	//@Pointcut("execution(* * org.prathyusha.model.Circle.*(..))")
+	//if we have 100 methods then it will return all methods
+	public void allCircleMethods() {}
+	
+	
+}
