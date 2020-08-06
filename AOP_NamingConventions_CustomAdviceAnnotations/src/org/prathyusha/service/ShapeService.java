587f8bbc1fa337a25098a037e748ea4e12eed601
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-06 20:11:51 +0530

    Add files via upload

---
+++ AOP_NamingConventions_CustomAdviceAnnotations/src/org/prathyusha/service/ShapeService.java
@@ -0,0 +1,29 @@
+package org.prathyusha.service;
+
+import org.prathyusha.aspect.Loggable;
+import org.prathyusha.model.Circle;
+import org.prathyusha.model.Triangle;
+
+public class ShapeService {
+  
+	private Circle circle;
+	private Triangle triangle;
+	
+	@Loggable
+	public Circle getCircle() {
+		return circle;
+	}
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	public Triangle getTriangle() {
+		return triangle;
+	}
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+	
+	
+	
+	
+}
